c5794ab0636376eef196f392030122cb89c4d4c3
Lilykano <132531503+Lilykano@users.noreply.github.com>
2023-06-14 09:48:19 +0900

    Add files via upload

---
+++ kano_Board/src/kano_Board/Main.java
@@ -0,0 +1,16 @@
+package kano_Board;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Board board=new Board();
+		board.run();
+	}
+
+}
